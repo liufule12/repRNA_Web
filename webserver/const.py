4d4792007fba83d9aa3706db9114746a41a276a3
liufule12 <liufule12@gmali.com>
2015-01-20 17:02:56 +0800

    add ALLOWED_EXTENSIONS

--- webserver/const.py
+++ webserver/const.py
@@ -1,6 +1,8 @@
 __author__ = 'aleeee'
 
 
+ALLOWED_EXTENSIONS = (['txt', 'fasta'])
+
 METHODS_PHYCHE_INDEX = ['DAC', 'DCC', 'DACC', 'PC-PseDNC-General', 'SC-PseDNC-General']
 METHODS_LAG = ['DAC', 'DCC', 'DACC']
 METHODS_LAMADA_W = ['PseSSC', 'PseDPC', 'PC-PseDNC-General', 'SC-PseDNC-General']
