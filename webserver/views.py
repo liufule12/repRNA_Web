065bc2fe82d30b06fae336bc29188c8e00863d96
liufule12 <liufule12@gmali.com>
2015-01-20 17:03:38 +0800

    add args trans, create folder.

--- webserver/views.py
+++ webserver/views.py
@@ -1,6 +1,10 @@
-from webserver import app
+import os
+import time
 
+from webserver import app
 from flask import Flask, render_template, redirect, request
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
 
 import webserver._method as _method
 
@@ -49,13 +53,33 @@ def main(mode):
         print("request.form", request.form)
         print("request.files", request.files)
 
-        # Check and transform the form args.
-        check_res = _method.tran_args(request.form, mode)
-        if check_res[0] is False:
-            return render_template('result.html', er_info=(True, check_res[1]))
+        # Transform the form args and add parameter k.
+        form_args = _method.tran_args(request.form, mode)
+        print("Args is ok.", form_args)
+
+        # Create the user fold.
+        user_ip_time = request.remote_addr + '_' + str(time.time())
+        user_dir = os.getcwd() + '/webserver/static/temp/' + user_ip_time
+        _method.create_user_fold(user_dir)
+        print("The user fold is ok.")
+
+        # Deal with the upload data file.
+        rec_upload_file = request.files['upload_data']
+        print(rec_upload_file)
+        if rec_upload_file and _method.allowed_file(rec_upload_file.filename):
+            upload_file = secure_filename(rec_upload_file.filename)
+            upload_file_path = user_dir + '/' + upload_file
+            rec_upload_file.save(upload_file_path, buffer_size=1)
+        elif rec_upload_file and not _method.allowed_file(rec_upload_file.filename):
+            return render_template("result.html",
+                                   er_info=(True, "Sorry, the upload file must be txt or fasta file."))
         else:
-            form_args = check_res[1]
-        print("Args is ok.")
+            upload_file = None
+        print("The user upload data file is ok.")
 
         return "Process in main completed."
 
+
+@app.route("/test/")
+def test():
+    return render_template("test.html")
