506abc6375a6f45434c970720b2cd0df0b6a1509
liufule12 <liufule12@gmali.com>
2015-01-23 17:04:38 +0800

    add METHDOS_PSE, ACC and RNA_ALP_NUM

--- webserver/const.py
+++ webserver/const.py
@@ -6,6 +6,10 @@ ARGS = ['rec_data', 'mode', 'k', 'd', 'n', 'lamada', 'lag', 'w']
 METHODS_PHYCHE_INDEX = ['DAC', 'DCC', 'DACC', 'PC-PseDNC-General', 'SC-PseDNC-General']
 METHODS_LAG = ['DAC', 'DCC', 'DACC']
 METHODS_LAMADA_W = ['PseSSC', 'PseDPC', 'PC-PseDNC-General', 'SC-PseDNC-General']
+METHODS_PSE = ['PC-PseDNC-General', 'SC-PseDNC-General']
+METHODS_ACC = ['DAC', 'DCC', 'DACC']
+
+RNA_ALP_NUM = 4
 
 ALPHABET_RNA = "ACGU"
 MAX_SEQ_NUM = 50
